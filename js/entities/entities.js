d21f055cb989ecaea88eb1a4b6dfaa11f7608a65
alexv510 <aovallad@ucsc.edu>
2015-03-06 19:11:01 -0800

    MAIN CHARACTER SPRITE
    IT MOVES UP AND DOWN CORRECTLY NAOS

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -30,10 +30,11 @@ game.PlayerEntity = me.Entity.extend({
 		this.renderable.addAnimation("run_left", [3,4,5]);
 		this.renderable.addAnimation("run_down", [0,1,2]);
 		this.renderable.addAnimation("run_up", [9, 10, 11]);
-		this.renderable.addAnimation("stand", [6]);
-		this.renderable.addAnimation("downstand", [0]);
-		this.renderable.addAnimation("upstand", [9]);
-		this.renderable.setCurrentAnimation("stand");
+		this.renderable.addAnimation("left_stand", [3]);
+		this.renderable.addAnimation("right_stand", [6]);
+		this.renderable.addAnimation("down_stand", [0]);
+		this.renderable.addAnimation("up_stand", [9]);
+		this.renderable.setCurrentAnimation("right_stand");
 		
     },
 
@@ -45,7 +46,6 @@ game.PlayerEntity = me.Entity.extend({
 		if(me.input.isKeyPressed('left')) {
 			this.renderable.flipX(true);
 			this.body.vel.x -= this.body.accel.x * me.timer.tick;
-			
 			if(!this.renderable.isCurrentAnimation("run_right")){
 				this.renderable.setCurrentAnimation("run_right");
 			}
@@ -58,15 +58,12 @@ game.PlayerEntity = me.Entity.extend({
 			}
 		}
 		else if(me.input.isKeyPressed('down')){
-			this.renderable.setCurrentAnimation("run_down");
-			this.renderable.flipY(false);
 			this.body.vel.y += this.body.accel.y*me.timer.tick;
-			if(!this.renderable.isCurrentAnimation("run_down")){
+			 if(!this.renderable.isCurrentAnimation("run_down")){
 				this.renderable.setCurrentAnimation("run_down");
 			}
 		}
 		else if(me.input.isKeyPressed('up')){
-			this.renderable.setCurrentAnimation("run_up");
 			this.body.vel.y -= this.body.accel.y* me.timer.tick;
 			if(!this.renderable.isCurrentAnimation("run_up")){
 				this.renderable.setCurrentAnimation("run_up");
@@ -75,7 +72,16 @@ game.PlayerEntity = me.Entity.extend({
 		else{
 			this.body.vel.x = 0;
 			this.body.vel.y=0;
-			this.renderable.setCurrentAnimation("stand");
+			
+			if(me.input.isKeyPressed("up")){
+				this.renderable.setCurrentAnimation("up_stand");
+			}
+			else if (me.input.isKeyPressed("down")){
+				this.renderable.setCurrentAnimation("down_stand");
+			}
+			else{
+				this.renderable.setCurrentAnimation("right_stand");
+			}
 		}
 
         // apply physics to the body (this moves the entity)
