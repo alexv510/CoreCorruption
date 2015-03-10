32c4b239d3abe3fe43e1afe2b5a835e6de334a39
Bryan Ambriz <bambriz@ucsc.edu>
2015-03-09 18:07:59 -0700

    Added temporary Level Song
    NEw level song

--- js/screens/play.js
+++ js/screens/play.js
@@ -13,6 +13,7 @@ game.PlayScreen = me.ScreenObject.extend({
         // add our HUD to the game world
         this.HUD = new game.HUD.Container();
         me.game.world.addChild(this.HUD);
+        me.audio.playTrack("LevelSong");
     },
 
     /**
@@ -21,5 +22,6 @@ game.PlayScreen = me.ScreenObject.extend({
     onDestroyEvent: function() {
         // remove the HUD from the game world
         me.game.world.removeChild(this.HUD);
+         me.audio.stopTrack();
     }
 });
