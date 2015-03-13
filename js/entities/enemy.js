68aacb3760d0b94b410613b419e080897b70a2df
alexv510 <aovallad@ucsc.edu>
2015-03-12 20:05:56 -0700

    update
    idk, past hour or so has been a blurr

--- js/entities/enemy.js
+++ js/entities/enemy.js
@@ -6,13 +6,13 @@ game.EnemyEntity = me.Entity.extend({
     // define this here instead of tiled
     settings.image = "wheelie_right";
 	settings.name = "enemy";
-     
+    this.touchSound = false; // is used to make sure sound is not repeated 
     // save the area size defined in Tiled
     var width = settings.width;
     var height = settings.height;
       this.moveV = false;
       this.verticalDir = -1;
- 
+	this.moveDir = 1;
     // adjust the size setting information to match the sprite size
     // so that the entity object is created with the right size
     settings.spritewidth = settings.width = 32;
@@ -23,8 +23,8 @@ game.EnemyEntity = me.Entity.extend({
      
     // set start/end position based on the initial area size
     x = this.pos.x;
-    this.startX = x;
-    this.endX   = x + width - settings.spritewidth
+    this.startX = x - 96;
+    this.endX   = x + width - settings.spritewidth + 96;
     this.pos.x  = x + width - settings.spritewidth;
       
       //animation stuff
@@ -48,27 +48,37 @@ game.EnemyEntity = me.Entity.extend({
  
   // manage the enemy movement
   update: function(dt) {
- 
+    
     if (this.alive) {
       if (this.walkLeft && this.pos.x <= this.startX) {
       this.walkLeft = false;
+          this.touchSound = false; // here is where I change it to false
     } else if (!this.walkLeft && this.pos.x >= this.endX) {
       this.walkLeft = true;
+        this.touchSound = false; // here too
     }
+        
+        if(this.pos.x - game.data.playerX <= 96 &&
+             ( this.pos.y - game.data.playerY <= 32 ||
+                this.pos.y - game.data.playerY >=32)){
+                this.moveDir = 1;
+        }else{
+              this.moveDir = 3;
+        }
     // make it walk
    // this.renderable.flipX(this.walkLeft);
    // this.body.vel.x += (this.walkLeft) ? -this.body.accel.x * me.timer.tick : this.body.accel.x * me.timer.tick;
-        if(this.walkLeft&& !this.moveV){
+        if(this.walkLeft&& !this.moveV || this.moveDir == 1){
             this.body.vel.x +=-this.body.accel.x * me.timer.tick;
             if(!this.renderable.isCurrentAnimation("run_left")){
         this.renderable.setCurrentAnimation("run_left");
             }
-        }else if(!this.moveV){
+        }else if(!this.moveV || this.moveDir == 3){
                     this.body.vel.x +=this.body.accel.x * me.timer.tick;
             if(!this.renderable.isCurrentAnimation("run_right")){
               this.renderable.setCurrentAnimation("run_right");
             }
-        }else if(this.moveV && this.verticalDir > 0){
+        }else if(this.moveV && this.verticalDir > 0 || this.moveDir == 2){
                 this.body.vel.y +=this.body.accel.y*me.timer.tick;
                 if(!this.renderable.isCurrentAnimation("run_down")){
               this.renderable.setCurrentAnimation("run_down");
@@ -89,6 +99,7 @@ game.EnemyEntity = me.Entity.extend({
      
     // handle collisions against other shapes
     me.collision.check(this);
+      
        
     // return true if we moved or if the renderable was updated
     return (this._super(me.Entity, 'update', [dt]) || this.body.vel.x !== 0 || this.body.vel.y !== 0);
@@ -102,25 +113,35 @@ game.EnemyEntity = me.Entity.extend({
     if (response.b.body.collisionType !== me.collision.types.WORLD_SHAPE) {
       // res.y >0 means touched by something on the bottom
       // which mean at top position for this one
-      if (this.alive && (response.overlapV.y > 0)  || (response.overlapV.x > 0)) {
-        this.renderable.flicker(750);
-      }
-        if((response.overlapV.y > 0) && response.b.type!== 'EnemyEntity'){
-             this.moveV = true;
-             this.verticalDir = this.verticalDir*-1;
-         }else if ((response.overlapV.x > 0)){
-                this.moveV = false;
-             this.verticalDir = this.verticalDir*-1;
+      if (this.alive && ((response.overlapV.y > 0)  || (response.overlapV.x > 0)) && response.b.body.collisionType === me.collision.types.PLAYER_OBJECT ) {
+       // this.renderable.flicker(750);
+          //here i removed flickering and added audio stuff
+         var choose = Math.floor((Math.random()*3)+1);
+         if(this.touchSound == false){
+          switch(choose){
+                case 1: me.audio.play("criminalscum", .5);
+                        this.touchSound = true;
+                        break;
+                case 2: me.audio.play("stoprest", .5); this.touchSound = true;break;
+                
+                case 3: me.audio.play("freezescumbag", .5);this.touchSound = true; break;
+                  
+                default: me.audio.play("stoprest");this.touchSound = true; break;
+          }
          }
+          
+      }
+         if ((response.overlapV.x > 0)&& response.b.body.collisionType !== me.collision.types.PLAYER_OBJECT){
+                //this.moveV = false;
+            this.walkLeft = this.walkLeft ? false: true;
+         }
+       
       return false;
     }
-      if((response.overlapV.y > 0)){
-             this.moveV = true;
-             this.verticalDir = this.verticalDir*-1;
-         }else if ((response.overlapV.x > 0)){
-                this.moveV = false;
-             this.verticalDir = this.verticalDir*-1;
+       if ((response.overlapV.x > 0)&& response.b.body.collisionType !== me.collision.types.PLAYER_OBJECT){
+                this.walkLeft = this.walkLeft ? false: true;
          }
+      //this.touchSound = false;
     // Make all other objects solid
     return true;
   }
